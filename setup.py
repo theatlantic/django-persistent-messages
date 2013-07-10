3848880391973006e63f7aa12921ca9ff54a6927
Frankie Dintino <fdintino@theatlantic.com>
2013-07-10 19:10:51 -0400

    Add atlantic-specific version to packages' setup.py/setup.cfg

--- setup.py
+++ setup.py
@@ -1,5 +1,14 @@
+#!/usr/bin/env python
+
 import os
-from distutils.core import setup
+
+try:
+    from setuptools import setup
+except ImportError:
+    from ez_setup import use_setuptools
+    use_setuptools()
+    from setuptools import setup
+
 
 def read(fname):
     return open(os.path.join(os.path.dirname(__file__), fname)).read()
