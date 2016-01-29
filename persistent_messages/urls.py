d1333399f17c3dda48285ce67aca22fabc4eb8bd
Frankie Dintino <fdintino@theatlantic.com>
2016-01-29 16:30:56 -0500

    Fix DeprecationWarning by changing django.conf.urls.patterns() to list

--- persistent_messages/urls.py
+++ persistent_messages/urls.py
@@ -1,8 +1,10 @@
-from django.conf.urls import patterns, url
+from django.conf.urls import url
+
+import persistent_messages.views
 
 
-urlpatterns = patterns('',
-    url(r'^detail/(?P<message_id>\d+)/$', 'persistent_messages.views.message_detail', name='message_detail'),
-    url(r'^mark_read/(?P<message_id>\d+)/$', 'persistent_messages.views.message_mark_read', name='message_mark_read'),
-    url(r'^mark_read/all/$', 'persistent_messages.views.message_mark_all_read', name='message_mark_all_read'),
-)
+urlpatterns = [
+    url(r'^detail/(?P<message_id>\d+)/$', persistent_messages.views.message_detail, name='message_detail'),
+    url(r'^mark_read/(?P<message_id>\d+)/$', persistent_messages.views.message_mark_read, name='message_mark_read'),
+    url(r'^mark_read/all/$', persistent_messages.views.message_mark_all_read, name='message_mark_all_read'),
+]
